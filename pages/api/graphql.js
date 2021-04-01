7806a6e5b64ba102fd3486d3f13aa86b8a7f9843
Jeremy Dryden <jeremy.dryden@gmail.com>
2021-03-31 22:36:10 -0400

    added the graphql playground

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -2,7 +2,12 @@ import { ApolloServer } from "apollo-server-micro";
 import { typeDefs } from "./schemas";
 import { resolvers } from "./resolvers";
 
-const apolloServer = new ApolloServer({ typeDefs, resolvers });
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true,
+});
 
 export const config = {
   api: {

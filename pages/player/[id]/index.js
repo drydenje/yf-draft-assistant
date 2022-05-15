adddacddb048c8f38e262325a214de751a96ad26
Jeremy Dryden <jeremy.dryden@gmail.com>
2022-05-14 23:38:55 -0400

    started the individual player page, going to close this branch and start another

---
+++ pages/player/[id]/index.js
@@ -0,0 +1,42 @@
+import { userRouter } from "next/router";
+import FetchGraphQL from "@/services/Data/FetchGraphQL";
+
+const player = ({ player }) => {
+  console.log(player);
+  return (
+    <div>
+      <span>{player.nameFirst}</span> <span>{player.nameLast}</span>
+    </div>
+  );
+};
+
+export const getServerSideProps = async (context) => {
+  const playerQuery = `
+  query {
+    playerByID(id: "${context.params.id}") {
+      playerID
+      nameFirst
+      nameLast
+      height
+      basicBattingStats {
+        yearID
+        iH
+      }
+      # basicPitchingStats {
+      #   year
+      #   ID
+      #   W
+      #   L
+      #   ERA
+      # }
+    }
+  }
+  `;
+
+  const res = await FetchGraphQL(playerQuery);
+  console.log(res);
+  const player = res.data.playerByID;
+  return { props: { player } };
+};
+
+export default player;

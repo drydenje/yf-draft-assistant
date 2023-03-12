0e92d1c7b415da7b7a43eeb1b5ba71aa39a8a1e0
Jeremy Dryden <jeremy.dryden@gmail.com>
2023-03-11 22:28:51 -0500

    added a table, tailwind styles are not being applied

--- pages/player.js
+++ pages/player.js
@@ -1,16 +1,20 @@
 import Head from "next/head";
+import Table from "@/components/Table";
 
 const Player = ({ player }) => {
   const { name } = player;
+  console.log(typeof name);
   return (
     <>
       <Head>
-        <title>{name}'s Statistics</title>
+        {/* Warning: A title element received an array with more than 1 element as children. */}
+        {/* <title>{name}'s Statistics</title> */}
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
 
       <h1>Players</h1>
       <h2>{name}</h2>
+      <Table />
     </>
   );
 };
@@ -22,13 +26,17 @@ export async function getStaticProps() {
   // const players = await res.json();
 
   // dummy data for development
-  const players = [
-    {
-      name: "Mike Trout",
-    },
-  ];
+  // const players = [
+  //   {
+  //     name: "Mike Trout",
+  //   },
+  // ];
 
-  const player = players[0];
+  // const player = players[0];
+
+  const player = {
+    name: "Mike Trout",
+  };
 
   return {
     props: {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./pages/**/*.{js, ts, jsx, tsx}",
     "./src/components/**/*.{js, ts, jsx, tsx}",
+    "./src/domain/**/*.{js, ts, jsx, tsx}",
+    "./src/stories/**/*.{js, ts, jsx, tsx}",
   ],
   theme: {
     extend: {},

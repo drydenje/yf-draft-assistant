10da01bcc06a4187bd9645ed37ba7fe5bfadbc52
Jeremy Dryden <jeremy.dryden@gmail.com>
2023-04-13 22:57:41 -0400

    added tables listing all players, separated into pitcher and hitter categories. There are redundant listings because most pitchers have atbats, might want to combine the two and have one giant list until we can do it properly

--- pages/franchise/[id]/index.tsx
+++ pages/franchise/[id]/index.tsx
@@ -9,58 +9,28 @@ const franchise = ({ franchise }) => {
     .concat('-logo.svg');
   const logoPath = "/assets/logos/" + svgName;
   const altText = "Logo of the " + franchise.franchName;
-
-  const batterHeadings = [
-    { Header: "Year", accessor: "yearID" },
-    { Header: "G", accessor: "iG" },
-    { Header: "AB", accessor: "iAB" },
-    { Header: "R", accessor: "iR" },
-    { Header: "H", accessor: "iH" },
-    { Header: "2B", accessor: "i2B" },
-    { Header: "3B", accessor: "i3B" },
-    { Header: "HR", accessor: "iHR" },
-    { Header: "RBI", accessor: "iRBI" },
-    { Header: "SB", accessor: "iSB" },
-    { Header: "CS", accessor: "iCS" },
-    { Header: "BB", accessor: "iBB" },
-    { Header: "SO", accessor: "iSO" },
-    { Header: "IBB", accessor: "iIBB" },
-    { Header: "HBP", accessor: "iHBP" },
-    { Header: "SH", accessor: "iSH" },
-    { Header: "SF", accessor: "iSF" },
-    { Header: "GIDP", accessor: "iGIDP" },
+  const { hitters, pitchers } = franchise.roster;
+  const headings = [
+    { Header: "nameFirst", accessor: "nameFirst" },
+    { Header: "nameLast", accessor: "nameLast" },
+    { Header: "nameGiven", accessor: "nameGiven" },
+    { Header: "playerID", accessor: "playerID" },
+    { Header: "birthYear", accessor: "birthYear" },
+    { Header: "birthMonth", accessor: "birthMonth" },
+    { Header: "birthDay", accessor: "birthDay" },
+    { Header: "birthCountry", accessor: "birthCountry" },
+    { Header: "birthState", accessor: "birthState" },
+    { Header: "birthCity", accessor: "birthCity" },
+    { Header: "weight", accessor: "weight" },
+    { Header: "height", accessor: "height" },
+    { Header: "bats", accessor: "bats" },
+    { Header: "throws", accessor: "throws" },
+    { Header: "debut", accessor: "debut" },
+    { Header: "finalGame", accessor: "finalGame" },
+    { Header: "retroID", accessor: "retroID" },
+    { Header: "bbrefID", accessor: "bbrefID" },
   ];
-  const pitcherHeadings = [
-    { Header: "yearID", accessor: "yearID" },
-    { Header: "stint", accessor: "stint" },
-    { Header: "teamID", accessor: "teamID" },
-    { Header: "lgID ", accessor: "lgID " },
-    { Header: "W", accessor: "W" },
-    { Header: "L", accessor: "L" },
-    { Header: "G", accessor: "G" },
-    { Header: "GS", accessor: "GS" },
-    { Header: "CG", accessor: "CG" },
-    { Header: "SHO", accessor: "SHO" },
-    { Header: "SV", accessor: "SV" },
-    { Header: "IPouts", accessor: "IPouts" },
-    { Header: "H", accessor: "H" },
-    { Header: "ER", accessor: "ER" },
-    { Header: "HR", accessor: "HR" },
-    { Header: "BB", accessor: "BB" },
-    { Header: "SO", accessor: "SO" },
-    { Header: "BAOpp", accessor: "BAOpp" },
-    { Header: "ERA", accessor: "ERA" },
-    { Header: "IBB", accessor: "IBB" },
-    { Header: "WP", accessor: "WP" },
-    { Header: "HBP", accessor: "HBP" },
-    { Header: "BK", accessor: "BK" },
-    { Header: "BFP", accessor: "BFP" },
-    { Header: "GF", accessor: "GF" },
-    { Header: "R", accessor: "R" },
-    { Header: "SH", accessor: "SH" },
-    { Header: "SF", accessor: "SF" },
-  ];
-
+  
   return (
     <div>
       <p>
@@ -71,19 +41,23 @@ const franchise = ({ franchise }) => {
         alt={altText}
         width={0}
         height={0}
-        // width={200}
-        // height={161.36}
         priority
         className="w-1/5 h-auto"
       />
-{/* 
-      {player.basicBattingStats.length > 0 ? (
-        <Table headings={batterHeadings} stats={player.basicBattingStats} />
+
+      {hitters.length > 0 ? (
+        <div>
+          <h2>Hitters</h2>
+          <Table headings={headings} stats={hitters} />
+        </div>
       ) : null}
-      {player.basicPitchingStats.length > 0 ? (
-        <Table headings={pitcherHeadings} stats={player.basicPitchingStats} />
+      {pitchers.length > 0 ? (
+        <div>
+          <h2>Pitchers</h2>
+          <Table headings={headings} stats={pitchers} />
+        </div>
       ) : null}
-       */}
+      
     </div>
   );
 };
@@ -96,6 +70,48 @@ export const getServerSideProps = async (context) => {
           franchName
           active
           NAassoc
+          roster {
+            hitters {
+              playerID
+              birthYear
+              birthMonth
+              birthDay
+              birthCountry
+              birthState
+              birthCity
+              nameFirst
+              nameLast
+              nameGiven
+              weight
+              height
+              bats
+              throws
+              debut
+              finalGame
+              retroID
+              bbrefID
+            }
+            pitchers {
+              playerID
+              birthYear
+              birthMonth
+              birthDay
+              birthCountry
+              birthState
+              birthCity
+              nameFirst
+              nameLast
+              nameGiven
+              weight
+              height
+              bats
+              throws
+              debut
+              finalGame
+              retroID
+              bbrefID
+            }
+          }
         }
       }
 `;

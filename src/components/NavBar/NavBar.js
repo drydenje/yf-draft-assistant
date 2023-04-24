486708f9d445c9df9f3c7286f62b16f4914a3bb4
Jeremy Dryden <jeremy.dryden@gmail.com>
2023-04-23 22:49:02 -0400

    got a basic dark mode switcher with extremely ugly colours

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,27 +1,31 @@
 import Link from "next/link";
+import Button from "@/components/Button";
 import styles from "./NavBar.module.css";
 
 const NavBar = () => {
   return (
-    <nav className={styles.nav}>
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/franchise">Franchises</Link>
-        </li>
-        <li>
-          <Link href="/league">Leagues</Link>
-        </li>
-        <li>
-          <Link href="/player">Players</Link>
-        </li>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className={styles.nav}>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/franchise">Franchises</Link>
+          </li>
+          <li>
+            <Link href="/league">Leagues</Link>
+          </li>
+          <li>
+            <Link href="/player">Players</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <Button />
+    </>
   );
 };
 

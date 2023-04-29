478e80a1624b8807a529b773905ba15f371fa4ad
Jeremy Dryden <jeremy.dryden@gmail.com>
2023-04-28 23:42:24 -0400

    removed default styles on iframe in storybook, got images working with addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,23 @@
 /** @type { import('@storybook/nextjs').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)", "../pages/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
+  stories: [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+    "../pages/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-mdx-gfm",
+  ],
   framework: {
     name: "@storybook/nextjs",
-    options: {}
+    options: {},
   },
   docs: {
-    autodocs: "tag"
-  }
+    autodocs: "tag",
+  },
+  staticDirs: ["../public"],
 };
 export default config;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,21 @@
+import Image from "next/image";
 import styles from "./Header.module.css";
 
 const Header = () => {
+  const imagePath = "/assets/test/swan.webp";
+  const altText = "Test";
+
   return (
     <header>
       <h1 className={styles.title}>Header</h1>
+      <Image
+        className={styles.image}
+        src={imagePath}
+        alt={altText}
+        width={100}
+        height={100}
+        priority
+      />
     </header>
   );
 };

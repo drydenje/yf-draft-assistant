eaa04b10c8e53da52d238fc59a6d7cd2f3c295a0
Jeremy Dryden <jeremy.dryden@gmail.com>
2023-05-02 21:42:01 -0400

    nextjs/fonts are working in the app, storybook next

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,16 +5,23 @@ import Layout from "@/domain/Layout";
 import "@/styles/normalize.css";
 import "@/styles/globals.css";
 import "@/styles/colours.css";
-import { Montserrat } from 'next/font/google';
+import { Montserrat, Oswald } from 'next/font/google';
 import { ThemeProvider } from "next-themes";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode
 }
 
+const oswald = Oswald({
+  variable: '--heading-font',
+  subsets:['latin'],
+  // weight: ['400', '700']
+})
+
 const montserrat = Montserrat({
+  variable: '--paragraph-font',
   subsets: ['latin'],
-  weight: ['400', '700'],
+  // weight: ['400', '700'],
 })
 
 type AppPropsWithLayout = AppProps & {
@@ -28,7 +35,7 @@ function App({ Component, pageProps }: AppPropsWithLayout) {
 
   return getLayout(
     <ThemeProvider>
-      <div className={montserrat.className}>
+      <div className={montserrat.variable + " " + oswald.variable}>
         <Layout>
           <Component {...pageProps}/>
         </Layout>
